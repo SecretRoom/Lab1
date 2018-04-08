a54a57d5768d2c1387c10e1a97dc27f979ae4b9e
SecretRoom <nikolas270699@gmail.com>
2018-04-08 16:35:37 +0300

    Output info 2

--- OpenWeatherMap.py
+++ OpenWeatherMap.py
@@ -8,19 +8,15 @@ weather = observation.get_weather()
 location = observation.get_location()
 translate = {'Rostov-on-Don': 'Ростов-на-Дону'}
 
-print('Страна :'+location.get_country())
-print ('Город :'+translate[location.get_name()])
-print('Долгота :' +str(location.get_lon()))
-print('Широта :' +str(location.get_lat()))
-print('Облачность :'+str(weather.get_clouds()))
-print('Статус :'+str(weather.get_detailed_status()))
-print('Дождь :'+str(weather.get_rain()))
-print('Снег :'+str(weather.get_snow()))
-print('Время :'+str(weather.get_reference_time('iso')))
-print('Статус :'+str(weather.get_status()))
-print('Восход :'+str(weather.get_sunrise_time('iso')))
-print('Закат :'+str(weather.get_sunset_time('iso')))
-print('Температура :'+str(weather.get_temperature('celsius')))
-print('Видимость ;'+str(weather.get_visibility_distance()))
-print('Изображение :'+weather.get_weather_icon_name())
-print('Ветер :'+str(weather.get_wind()))
+def WhatIsCloudness():
+    if 0 <= weather.get_clouds() <=10:
+        return 'ясная'
+    if 10 < weather.get_clouds() <= 30:
+        return 'немного облачная'
+    if 30 < weather.get_clouds() <= 70:
+        return 'пасмурная'
+    if 70 < weather.get_clouds() <= 100:
+        return 'мрачная'
+print('Погода в городе ' + translate[location.get_name()] + ' ' +
+    WhatIsCloudness() + ', температура ' + str(weather.get_temperature('celsius')['temp']) + ' градусов цельсия'+
+    'скорость ветра ' + str(weather.get_wind()['speed']) + ' m/c.')
